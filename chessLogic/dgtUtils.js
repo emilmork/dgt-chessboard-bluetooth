6d6cf4178b32846085c14dc32415783229a6120b
Erik Gustafsson <erik.gustafsson@bekk.no>
2019-11-12 12:08:56 +0100

    Parse hex string instead of byte array.

--- chessLogic/dgtUtils.js
+++ chessLogic/dgtUtils.js
@@ -1,26 +1,25 @@
 const pieces = {
-  0: "_",
-  1: "P",
-  2: "R",
-  3: "N",
-  4: "B",
-  5: "K",
-  6: "Q",
-  7: "p",
-  8: "r",
-  9: "n",
-  10: "b",
-  11: "k",
-  12: "q"
+  "00": "_",
+  "01": "P",
+  "02": "R",
+  "03": "N",
+  "04": "B",
+  "05": "K",
+  "06": "Q",
+  "07": "p",
+  "08": "r",
+  "09": "n",
+  "10": "b",
+  "11": "k",
+  "12": "q"
 };
 
 export function parseBoardDump(data) {
   [...Array(8).keys()]
     .map(i =>
       data
-        .slice(i * 8, (i + 1) * 8)
-        .map(d => piece[d])
-        .join("")
+        .substring(i * 16, (i + 1) * 16)
+        .replace(/\d{2}/g, match => pieces[match])
         .replace(/(?<!_)_+(?!_)/g, match => match.length.toString())
     )
     .join("/");
