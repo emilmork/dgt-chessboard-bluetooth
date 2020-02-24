6d90145c3e866bf5d73abcf7213af9d1f29cf6f7
Emil Mork <emil.a.mork@gmail.com>
2020-02-24 20:01:45 +0100

    update service

--- service.sh
+++ service.sh
@@ -5,10 +5,10 @@ export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 
 case "$1" in
 start)
-exec forever --sourceDir=~/git/dgt-cheessboard-bluetooth -p ~/git/dgt-cheessboard-bluetooth server.js  #scriptarguments
+exec forever --sourceDir=/home/pi/dgt-cheessboard-bluetooth -p /home/pi/git/dgt-cheessboard-bluetooth server.js  #scriptarguments
 ;;
 stop)
-exec forever stop --sourceDir=~/git/dgt-cheessboard-bluetooth server.js
+exec forever stop --sourceDir=/home/pi/git/dgt-cheessboard-bluetooth server.js
 ;;
 *)
 echo "Usage: /etc/init.d/myService {start|stop}"
